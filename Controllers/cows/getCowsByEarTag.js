31ef3bbbb1fb76017b7b357dd99d957ebbf5286d
ThBromen <hashimwimanatheogene34@gmail.com>
2023-12-15 09:19:16 +0200

    new !

--- Controllers/cows/getCowsByEarTag.js
+++ Controllers/cows/getCowsByEarTag.js
@@ -2,13 +2,14 @@ import { Cow } from "../../Models";
 import { catchAsync } from "../Error/catchAsync";
 
 
-export const getCowByEarTag = catchAsync(async (req, res) => {
+export const getCowByEarTag = catchAsync(async (req, res, next) => {
     let requestEarTag = req.params.earTag;
-    let data = await Cow.find({ type: requestEarTag });
+    let data = await Cow.findOne({ earTag: requestEarTag });
 
     if (!data) {
-        return next(new AppError("no cow found with that EarTag", 404));
+        return next(new AppError("No cow found with that earTag", 404));
     }
-    console.log("the All Cow  is selected with EarTag:", data._id);
+
+    console.log("The cow is selected with earTag:", data.earTag);
     res.status(200).json(data);
 });
